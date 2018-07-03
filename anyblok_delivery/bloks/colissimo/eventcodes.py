41904fc2b68790d87d76d5a454e4c2f6de2df04d
jssuzanne <jssuzanne@anybox.fr>
2018-07-03 08:45:15 +0200

    Add new message for DOM / DOS product code: LIVVOI

--- anyblok_delivery/bloks/colissimo/eventcodes.py
+++ anyblok_delivery/bloks/colissimo/eventcodes.py
@@ -145,4 +145,7 @@ eventCodes = {
     # Votre colis ne peut être livré ce jour, l'accès à l'adresse de livraison
     # étant impossible. Il sera remis en livraison au plus tôt.
     'RSTFHB': 'exception',
+    # Votre colis est livré au voisin indiqué sur l'avis déposé dans la boîte
+    # aux lettres du destinataire.
+    "LIVVOI": "delivered",
 }
