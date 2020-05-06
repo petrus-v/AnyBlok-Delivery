91cfa3176010b83f746a271ac5160ca2f6511bdf
jssuzanne <js.suzanne@gmail.com>
2020-05-06 16:35:11 +0200

    Release 1.4.0

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ import os
 
 
 here = os.path.abspath(os.path.dirname(__file__))
-version = '1.3.0'
+version = '1.4.0'
 
 with open(os.path.join(here, 'README.rst'),
           'r', encoding='utf-8') as readme_file:
